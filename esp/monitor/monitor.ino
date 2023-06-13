e4ae41bb771fff95ae588d94244a9277228443eb
Johannes <johannes.bluhm@outlook.com>
2023-06-13 17:05:19 +0200

    generate sensor data

--- esp/monitor/monitor.ino
+++ esp/monitor/monitor.ino
@@ -1,89 +1,55 @@
-////////////////////////////////////////////////////////////////////////
-/// Copyright (c)2015 Dan Truong
-/// Permission is granted to use this software under the MIT
-/// licence, with my name and copyright kept in source code
-/// http://http://opensource.org/licenses/MIT
-///
-/// KY039 Arduino Heartrate Monitor V1.0 (April 02, 2015)
-////////////////////////////////////////////////////////////////////////
-
-// German Comments by Joy-IT
-
-int rawValue;
-
-
-bool heartbeatDetected(int IRSensorPin, int delay) {
-  static int maxValue = 0;
-  static bool isPeak = false;
-
-
-  bool result = false;
-
-  rawValue = analogRead(IRSensorPin);
-  // Hier wird der aktuelle Spannungswert am Fototransistor ausgelesen und in der rawValueVariable zwischengespeichert
-  rawValue *= (1000 / delay);
-
-  // Sollte der aktuelle Wert vom letzten maximalen Wert zu weit abweichen
-  // (z.B. da der Finger neu aufgesetzt oder weggenommen wurde)
-  // So wird der MaxValue resetiert, um eine neue Basis zu erhalten.
-  if (rawValue * 4L < maxValue) { maxValue = rawValue * 0.8; }  // Detect new peak
-  if (rawValue > maxValue - (1000 / delay)) {
-    // Hier wird der eigentliche Peak detektiert. Sollte ein neuer RawValue groeßer sein
-    // als der letzte maximale Wert, so wird das als Spitze der aufgezeichnten Daten erkannt.
-    if (rawValue > maxValue) {
-      maxValue = rawValue;
-    }
-    // Zum erkannten Peak soll nur ein Herzschlag zugewiesen werden
-    if (isPeak == false) {
-      result = true;
-    }
-    isPeak = true;
-  } else if (rawValue < maxValue - (3000 / delay)) {
-    isPeak = false;
-    // Hierbei wird der maximale Wert bei jeden Durchlauf
-    // etwas wieder herabgesetzt. Dies hat den Grund, dass
-    // nicht nur der Wert sonst immer stabil bei jedem Schlag
-    // gleich oder kleiner werden wuerde, sondern auch,
-    // falls der Finger sich minimal bewegen sollte und somit
-    // das Signal generell schwaecher werden wuerde.
-    maxValue -= (1000 / delay);
-  }
-  return result;
-}
-int ledPin = 13;
-int analogPin = 0;
+int pin = A0;
 
 void setup() {
-  // Die eingebaute Arduino LED (Digital 13), wird hier zur Ausgabe genutzt
-  pinMode(ledPin, OUTPUT);
-
-  // Serielle Ausgabe Initialisierung
-  Serial.begin(9600);
-  Serial.println("Heartbeat Detektion Beispielcode.");
+  Serial.begin(115200);
+  Serial.println("Setting up");
 }
 
-const int delayMsec = 60;  // 100msec per sample
-
-// Das Hauptprogramm hat zwei Aufgaben:
-// - Wird ein Herzschlag erkannt, so blinkt die LED kurz aufgesetzt
-// - Der Puls wird errechnet und auf der serriellen Ausgabe ausgegeben.
 
 void loop() {
-  // static int beatMsec = 0;
-  // int heartRateBPM = 0;
- if (heartbeatDetected(analogPin, delayMsec)) {
-    // heartRateBPM = 60000 / beatMsec;
-    // LED-Ausgabe bei Herzschlag
-    //digitalWrite(ledPin, 1);
+  int value = fakeAnalogRead();
+  Serial.println(value);
+  delay(10);
+}
 
-    // Serielle Datenausgabe
-    Serial.print("Puls erkannt: ");
-    // Serial.println(heartRateBPM);
+// https://ecgwaves.com/topic/ecg-normal-p-wave-qrs-complex-st-segment-t-wave-j-point/
+// When no finger is placed, the value is zero
+// When a finger is placed the value is around 2300 +- 100
+// When a pulse is detected the value is around 2800 +- 100
+unsigned long nextInterval = 0;
 
-    // beatMsec = 0;
-  } else {
-    //digitalWrite(ledPin, 0);
+const int rrIntervall = 880;
+const int qrsDuration = 120;
+
+const int noFingerValue = 0;
+const int fingerBaseValue = 2300;
+const int fingerPulseAdditionValue = 500;
+const float sensorVariance = 0.03;
+
+// Percentage from which the timings differ
+const float pulseVariance = 0.10;
+
+bool isPeak = false;
+bool hasFinger = true;
+
+int fakeAnalogRead() {
+  if (!hasFinger) {
+    return 0;
   }
-  //delay(delayMsec);
-  //beatMsec += delayMsec;
+
+  unsigned long currentMillis = millis();
+  int sensorValue = random(fingerBaseValue - (fingerBaseValue * sensorVariance), fingerBaseValue + (fingerBaseValue * sensorVariance));
+  if (isPeak) {
+    if (currentMillis >= nextInterval) {
+      nextInterval = currentMillis + random(rrIntervall - (rrIntervall * pulseVariance), rrIntervall + (rrIntervall * pulseVariance));
+      isPeak = false;
+    }
+    sensorValue = sensorValue + random(fingerPulseAdditionValue - (fingerPulseAdditionValue * sensorVariance), fingerPulseAdditionValue + (fingerPulseAdditionValue * sensorVariance));
+  } else {
+    if (currentMillis >= nextInterval) {
+      nextInterval = currentMillis + random(qrsDuration - (qrsDuration * pulseVariance), qrsDuration + (qrsDuration * pulseVariance));
+      isPeak = true;
+    }
+  }
+  return sensorValue;
 }
