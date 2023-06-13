dfb311e0d54463231555495b9a7dbdaf44508740
Johannes <johannes.bluhm@outlook.com>
2023-06-13 17:55:42 +0200

    calculate bpm

--- esp/monitor/monitor.ino
+++ esp/monitor/monitor.ino
@@ -3,9 +3,61 @@ void setup() {
   Serial.println("Setting up");
 }
 
+const int threshHoldFinger = 2000;
+const int threshHoldBeat = 2700;
+
+bool fingerDetected = false;
+const int beatsArrayLength = 4;
+int lastBeatsArray[beatsArrayLength];
+unsigned long lastBeatTime = 0;
 
 void loop() {
-  int value = fakeAnalogRead();
-  Serial.println(value);
   delay(10);
+  int value = fakeAnalogRead();
+
+  if (value < threshHoldFinger) {
+    fingerDetected = false;
+    Serial.println("Please place the finger on the sensor");
+  } else if (value > threshHoldFinger && value < threshHoldBeat) {
+    fingerDetected = true;
+  } else if (value > threshHoldBeat) {
+    fingerDetected = true;
+
+    // Wait until peak is over
+    while (fakeAnalogRead() > threshHoldBeat) {
+      delay(10);
+    }
+
+    // Shift array to left
+    for (int i = 0; i < beatsArrayLength - 1; i++) {
+      lastBeatsArray[i] = lastBeatsArray[i + 1];
+    }
+
+    // Push current timestamp to array
+    lastBeatsArray[beatsArrayLength - 1] = millis() - lastBeatTime;
+
+    lastBeatTime = millis();
+
+    // Check if enough variables to calculate the BPM are avaiable
+    bool canCalculate = true;
+    for (int i = 0; i < beatsArrayLength - 1; i++) {
+      if (lastBeatsArray[i] < 1) {
+        canCalculate = false;
+        break;
+      }
+    }
+
+    if (canCalculate) {
+      int val = 0;
+      for (int i = 0; i < beatsArrayLength; i++) {
+        val += lastBeatsArray[i];
+      }
+
+      val = (val / (beatsArrayLength - 1)) / 1000 * 60;
+      Serial.print("Current BPM: ");
+      Serial.println(val);
+    } else {
+      Serial.println("Waiting for enough data to calculate BPM");
+    }
+  }
 }
